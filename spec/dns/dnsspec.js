b74888f3a61845adb1316e854ca1bc84092389f4
Loren M. Lang <lorenl@north-winds.org>
2020-07-24 00:27:44 -0700

    Added a packer to more easily generate DNS test data

--- spec/dns/dnsspec.js
+++ spec/dns/dnsspec.js
@@ -154,4 +154,63 @@ describe("lib", function() {
       expect(name).toBe('child.ns.test.com.');
       expect(offset).toBe(i);
   });
+
+  it("should decode a DNS record header with multi-level compression", function() {
+      var buf = new ArrayBuffer(64);
+      var view = new DataView(buf);
+
+      class Packer {
+          constructor(view) {
+              this.view = view;
+              this.offset = 0;
+          }
+
+          packLabel(label) {
+              if(label == '.') {
+                  label = '';
+              }
+              this.view.setUint8(this.offset++, label.length);
+              for(var c in label) {
+                  this.view.setUint8(this.offset++, label.charCodeAt(c));
+              }
+          }
+
+          packLabels(labels) {
+              for(label in labels) {
+                  this.packLabel(label);
+              }
+          }
+
+          packPointer(pointer) {
+              this.view.setUint16(this.offset, 0xc000 | pointer);
+              this.offset += 2;
+          }
+
+          getOffset() {
+              return this.offset;
+          }
+      }
+
+      var packer = new Packer(view);
+      packer.packLabel('delete');
+      var www_offset = packer.getOffset();
+      packer.packLabel('www');
+      var test_offset = packer.getOffset();
+      packer.packLabel('test');
+      packer.packLabel('com');
+      packer.packLabel('');
+      var ns_offset = packer.getOffset();
+      packer.packLabel('ns');
+      packer.packPointer(test_offset);
+      var start_offset = packer.getOffset();
+      packer.packLabel('child');
+      packer.packPointer(ns_offset);
+
+      var [offset, name] = decodeName(view, start_offset, view);
+      expect(name).toBe('child.ns.test.com.');
+      expect(offset).toBe(packer.getOffset());
+
+      var [offset, name] = decodeName(view, www_offset, view);
+      expect(name).toBe('www.test.com.');
+  });
 });
